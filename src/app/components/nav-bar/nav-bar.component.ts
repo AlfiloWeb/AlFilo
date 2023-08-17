745bcaccad02df91d53cf2c6a17a0bbcf8c926f4
Lother88 <manrik88@gmail.com>
2023-08-17 20:01:46 +0200

    multiple fix

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -44,104 +44,104 @@ export class NavBarComponent {
 
   loginWithDiscord() {
     const clientId = '1122911967934414971';
-    const redirectUri = encodeURIComponent('http://localhost:4200/auth/callback');
+    const redirectUri = encodeURIComponent(
+      'http://localhost:4200/auth/callback'
+    );
     const scopes = encodeURIComponent('identify email');
     window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scopes}`;
   }
-
-
 
   // ngOnDestroy() {
   //   this.subscription.unsubscribe();
   // }
 
-   ngOnInit() {
-     this.generateToken();
-   }
+  ngOnInit() {
+    this.generateToken();
+  }
 
-   login() {
-     var aRerirectUrl = window.location.href;
-     window.location.href =
-       'https://api.staging.alfilo.org/signIn?aRedirectUrl=' +
-       encodeURIComponent(aRerirectUrl);
-   }
+  login() {
+    var aRerirectUrl = window.location.href;
+    window.location.href =
+      'https://api.staging.alfilo.org/signIn?aRedirectUrl=' +
+      encodeURIComponent(aRerirectUrl);
+  }
 
-   generateToken() {
-     if (localStorage.getItem('alfilo_token') === null) {
-       let headers = new HttpHeaders({
-         'Content-Type': 'application/json',
-       });
-       this.http
-         .get<any>('https://api.staging.alfilo.org/token', {
-           headers,
-           withCredentials: true,
-         })
-         .subscribe((resp) => {
-           localStorage.setItem('alfilo_token', resp);
-           this.isLogin = true;
-         });
-     }
-   }
+  generateToken() {
+    if (localStorage.getItem('alfilo_token') === null) {
+      let headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+      });
+      this.http
+        .get<any>('https://api.staging.alfilo.org/token', {
+          headers,
+          withCredentials: true,
+        })
+        .subscribe((resp) => {
+          localStorage.setItem('alfilo_token', resp);
+          this.isLogin = true;
+        });
+    }
+  }
 
-   getToken() {
-     return localStorage.getItem('alfilo_token');
-   }
+  getToken() {
+    return localStorage.getItem('alfilo_token');
+  }
 
-   removeToken() {
-     localStorage.removeItem('alfilo_token');
-   }
+  removeToken() {
+    localStorage.removeItem('alfilo_token');
+  }
 
-   logout() {
-     let headers = new HttpHeaders({
-       Authorization: 'Bearer ' + this.getToken(),
-       'Content-Type': 'application/json',
-     });
-     this.http
-       .get<any>('https://api.staging.alfilo.org/singOut', { headers })
-       .subscribe((resp) => {
-         this.removeToken();
-         this.isLogin = false;
-       });
-   }
+  logout() {
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.getToken(),
+      'Content-Type': 'application/json',
+    });
+    this.http
+      .get<any>('https://api.staging.alfilo.org/singOut', { headers })
+      .subscribe((resp) => {
+        this.removeToken();
+        this.isLogin = false;
+      });
+  }
 
-   loginTestAlfilo() {
-     this.checkHighestRol('loginTestAfiliado');
-   }
+  loginTestAlfilo() {
+    this.checkHighestRol('loginTestAfiliado');
+  }
 
-   loginTestCadete() {
-     this.checkHighestRol('loginTestCadete');
-   }
+  loginTestCadete() {
+    this.checkHighestRol('loginTestCadete');
+  }
 
-   loginTestDaga() {
-     this.checkHighestRol('loginTestDaga');
-   }
+  loginTestDaga() {
+    this.checkHighestRol('loginTestDaga');
+  }
 
-   loginEspada() {
-     this.checkHighestRol('loginTestEspada');
-   }
+  loginEspada() {
+    this.checkHighestRol('loginTestEspada');
+  }
 
-   loginAdmin() {
-     this.checkHighestRol('loginTestAdmin');
-   }
+  loginAdmin() {
+    this.checkHighestRol('loginTestAdmin');
+  }
 
-   checkHighestRol(method: string) {
-     let headers = new HttpHeaders({
-       Authorization: 'Bearer ' + this.getToken(),
-       'Content-Type': 'application/json',
-     });
-     this.http
-       .get<any>('https://api.staging.alfilo.org/' + method, { headers })
-       .subscribe({
-         next: (result) => {
-           this.textResult = result;
-         },
-         error: (error: HttpErrorResponse) => {
-           if (error.status !== HttpStatusCode.Forbidden) {
-             console.log(error.message);
-             return;
-           }
-           this.textResult = 'Este no es tu rol más alto, prueba con otro';
-         },
-       });
-   }
+  checkHighestRol(method: string) {
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.getToken(),
+      'Content-Type': 'application/json',
+    });
+    this.http
+      .get<any>('https://api.staging.alfilo.org/' + method, { headers })
+      .subscribe({
+        next: (result) => {
+          this.textResult = result;
+        },
+        error: (error: HttpErrorResponse) => {
+          if (error.status !== HttpStatusCode.Forbidden) {
+            console.log(error.message);
+            return;
+          }
+          this.textResult = 'Este no es tu rol más alto, prueba con otro';
+        },
+      });
+  }
 }
