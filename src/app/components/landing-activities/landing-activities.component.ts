1e485159eaf374dff24b59c60d750d580d74d3d7
Lother88 <manrik88@gmail.com>
2023-08-18 21:11:07 +0200

    add video backgound

--- src/app/components/landing-activities/landing-activities.component.ts
+++ src/app/components/landing-activities/landing-activities.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-landing-activities',
   templateUrl: './landing-activities.component.html',
   styleUrls: ['./landing-activities.component.css']
 })
-export class LandingActivitiesComponent {
+export class LandingActivitiesComponent implements AfterViewInit {
 
+  @ViewChild('myVideo') videoElement!: ElementRef;
+
+  ngAfterViewInit() {
+
+    this.videoElement.nativeElement.muted = true;
+
+  }
 }
