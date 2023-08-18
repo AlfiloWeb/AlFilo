1ecc89c847b4e7f3c17c83e7ddeb95c52f38328e
Lother88 <manrik88@gmail.com>
2023-08-18 19:50:15 +0200

    add basic cards on landing join

--- src/app/components/basic-card/basic-card.component.ts
+++ src/app/components/basic-card/basic-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-basic-card',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./basic-card.component.css']
 })
 export class BasicCardComponent {
-
+  @Input() title: string = '';
+  @Input() body: string = '';
 }
