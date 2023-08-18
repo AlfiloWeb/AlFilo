874e9e1100b9643ef0a59638e3eb9956f2502399
Lother88 <manrik88@gmail.com>
2023-08-18 20:31:05 +0200

    generaete component landing activities

---
+++ src/app/components/landing-activities/landing-activities.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-activities',
+  templateUrl: './landing-activities.component.html',
+  styleUrls: ['./landing-activities.component.css']
+})
+export class LandingActivitiesComponent {
+
+}
