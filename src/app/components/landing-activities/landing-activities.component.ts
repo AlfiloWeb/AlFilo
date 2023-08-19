f9f5802225bc9d98dc018c56867761e1d0a8da1c
Lother88 <manrik88@gmail.com>
2023-08-19 17:53:45 +0200

    add cards for all activities and click event

--- src/app/components/landing-activities/landing-activities.component.ts
+++ src/app/components/landing-activities/landing-activities.component.ts
@@ -7,11 +7,57 @@ import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 })
 export class LandingActivitiesComponent implements AfterViewInit {
 
+  combatSelected: boolean = true;
+  transportSelected: boolean = false;
+  explorationSelected: boolean = false;
+  industrialSelected: boolean = false;
+  supportSelected: boolean = false;
+  competitionSelected: boolean = false;
+
   @ViewChild('myVideo') videoElement!: ElementRef;
 
   ngAfterViewInit() {
 
     this.videoElement.nativeElement.muted = true;
 
+  }
+
+  resetActivity(){
+    this.combatSelected = false;
+    this.transportSelected = false;
+    this.explorationSelected = false;
+    this.industrialSelected = false;
+    this.supportSelected = false;
+    this.competitionSelected = false;
+  }
+
+  changeActivity(activity: string) {
+    this.resetActivity()
+    switch(activity) {
+      case 'transport': {
+        this.transportSelected = true;
+        break;
+      }
+      case 'exploration': {
+        this.explorationSelected = true;
+        break;
+      }
+      case 'industrial': {
+        this.industrialSelected = true;
+        break;
+      }
+      case 'support': {
+        this.supportSelected = true;
+        break;
+      }
+      case 'competition': {
+        this.competitionSelected = true;
+        break;
+      }
+      default: {
+        this.combatSelected = true;
+        break;
+      }
+   }
   }
 }
