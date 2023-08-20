c69665c2f2de3d261800ff0ed95487c568783f33
Lother88 <manrik88@gmail.com>
2023-08-20 16:53:26 +0200

    add chronology-card component

---
+++ src/app/components/chronology-card/chronology-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, booleanAttribute } from '@angular/core';
+
+@Component({
+  selector: 'app-chronology-card',
+  templateUrl: './chronology-card.component.html',
+  styleUrls: ['./chronology-card.component.css']
+})
+export class ChronologyCardComponent {
+  @Input() title: string = '';
+  @Input() body: string = '';
+  @Input() srcImage: string = '';
+  @Input({ transform: booleanAttribute }) left: boolean;
+}
