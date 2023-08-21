0ef2edce0cb9dba0ed2df8c64c065a17c1408d01
David Jiménez Monterrubio <28drew05@gmail.com>
2023-08-21 12:31:01 +0200

    added discord svg + discord button animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { WikiComponent } from './components/wiki/wiki.component';
-import { AppRoutingModule } from './app.routing.module';
-import { FooterComponent } from './components/footer/footer.component';
-import { JoinCardComponent } from './components/join-card/join-card.component';
-import { BasicCardComponent } from './components/basic-card/basic-card.component';
-import { LandingJoinComponent } from './components/landing-join/landing-join.component';
-import { JoinInfoComponent } from './components/join-info/join-info.component';
-import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
-import { LandingActivitiesComponent } from './components/landing-activities/landing-activities.component';
-import { LandingChronologyComponent } from './components/landing-chronology/landing-chronology.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
+import {AppComponent} from './app.component';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
+import {LandingComponent} from './components/landing/landing.component';
+import {WikiComponent} from './components/wiki/wiki.component';
+import {AppRoutingModule} from './app.routing.module';
+import {FooterComponent} from './components/footer/footer.component';
+import {JoinCardComponent} from './components/join-card/join-card.component';
+import {BasicCardComponent} from './components/basic-card/basic-card.component';
+import {LandingJoinComponent} from './components/landing-join/landing-join.component';
+import {JoinInfoComponent} from './components/join-info/join-info.component';
+import {AuthCallbackComponent} from './components/auth-callback/auth-callback.component';
+import {LandingActivitiesComponent} from './components/landing-activities/landing-activities.component';
+import {LandingChronologyComponent} from './components/landing-chronology/landing-chronology.component';
+import {SvgDiscordComponent} from "./components/svgs/discord-svg/svg-discord.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    LandingComponent,
-    WikiComponent,
-    FooterComponent,
-    JoinCardComponent,
-    BasicCardComponent,
-    LandingJoinComponent,
-    JoinInfoComponent,
-    AuthCallbackComponent,
-    LandingActivitiesComponent,
-    LandingChronologyComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavBarComponent,
+        LandingComponent,
+        WikiComponent,
+        FooterComponent,
+        JoinCardComponent,
+        BasicCardComponent,
+        LandingJoinComponent,
+        JoinInfoComponent,
+        AuthCallbackComponent,
+        LandingActivitiesComponent,
+        LandingChronologyComponent,
+        SvgDiscordComponent,
+        SvgDiscordComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
