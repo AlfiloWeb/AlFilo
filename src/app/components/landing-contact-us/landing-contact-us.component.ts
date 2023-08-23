2580fa895f6e3690bf070fdf4a5f2d6714136170
Lother88 <manrik88@gmail.com>
2023-08-23 21:51:06 +0200

    new section contact us

---
+++ src/app/components/landing-contact-us/landing-contact-us.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-contact-us',
+  templateUrl: './landing-contact-us.component.html',
+  styleUrls: ['./landing-contact-us.component.css']
+})
+export class LandingContactUsComponent {
+
+}
