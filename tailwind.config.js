3093c38dc5fb05c82ec789aeb203b6fd1270adb9
Lother88 <manrik88@gmail.com>
2023-08-26 21:57:57 +0200

    first iteration mobile creators and change configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      maxWidth: {
-        '2k': '2560px',
+      screens: {
+        "xxs": "320px",
+        "xs": "430px",
       },
       colors: {
         "alfilo-gray": '#262626',
         "alfilo-blue": '#8cb9ca',
         "alfilo-dark-blue": '#0394cc',
         "discord": '#7289da',
-      }
+      },
     }
   },
   plugins: [require("daisyui")],
@@ -20,23 +21,13 @@ module.exports = {
     themes: [
       {
         mytheme: {
-
           "base-100": "#000000",
           "base-200": "#0394cc",
           "base-300": "#8cb9ca",
           "secondary": "#262626",
           "base-content": "#ffffff",
-
           "accent": "#0394cc"
-
         },
-        // screens: {
-        //   sm: "640px",
-        //   md: "768px",
-        //   lg: "1024px",
-        //   xl: "1280px",
-        //   "2xl": "1536px",
-        // },
       },
     ],
   },
