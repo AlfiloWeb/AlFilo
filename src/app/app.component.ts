6a8b67067de89e09b3a8c49fa02adcd0dd7f8209
IagoAres <iagoares97@gmail.com>
2023-11-17 17:31:47 +0100

    "Modal for IAE 2953 finally working"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AlFilo';
+export class AppComponent implements OnInit {
+title = 'AlFilo';
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.openDialog();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ModalComponent, {
+      // Set the width as per your requirements
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
