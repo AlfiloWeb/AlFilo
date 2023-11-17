65a6513c0a9e795bb26a54c51e9d0d42104d1d84
IagoAres <iagoares97@gmail.com>
2023-11-17 16:26:14 +0100

    Modal trial

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent {
+
+  constructor(public dialogRef: MatDialogRef<ModalComponent>) { }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+}
